44f26b9e940f4514275b5ebf3e0a08484633a8d0
HUGOOAT <152922166+HUGOOAT@users.noreply.github.com>
2024-01-12 11:00:39 +0100

    Ajout fonctions affichage résultats
    Ajout permettant la communication et l'affichage des résultats provenant du fichier seo.

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ class Gui:
     def __init__(self, fenetre):
         self.fenetre = fenetre
         self.fenetre.title("Analyseur SEO")
+        self.resultats = None
 
         self.url_label = tk.Label(fenetre, text="URL:")
         self.url_entry = tk.Entry(fenetre, width=30)
 
-        self.mots_label = tk.Label(fenetre, text="Mots clés (séparés par des virgules):")
+        self.mots_label = tk.Label(fenetre, text="Mots clés (séparés par des espaces):")
         self.mots_entry = tk.Entry(fenetre, width=30)
 
         self.bouton_analyse = tk.Button(fenetre, text="Analyser", command=self.lancer_analyse)
@@ -23,14 +24,18 @@ class Gui:
 
     def lancer_analyse(self):
         url = self.url_entry.get()
-        motscles = self.mots_entry.get().split(',')
+        motscles = self.mots_entry.get().split(' ')
 
-        analyseur = SEOAnalyser(url)
-
+        analyseur = SEOAnalyser(url, motscles)
+        self.resultats = analyseur.analyse_seo()
         self.afficher_resultats()
 
     def afficher_resultats(self):
-        messagebox.showinfo("Résultats", "Affichage des résultats")
+        if self.resultats is not None:
+            messagebox.showinfo("Résultats", self.resultats)
+        else:
+            messagebox.showwarning("Avertissement", "Lancez d'abord l'analyse.")
+
 
 if __name__ == "__main__":
     fenetre_principale = tk.Tk()
